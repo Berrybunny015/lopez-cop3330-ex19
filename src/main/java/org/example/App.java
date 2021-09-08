3020f4bb904ba2b9325e54f1316230f908543079
Berrybunny015 <berrybunny015@gmail.com>
2021-09-08 15:52:10 -0400

    extra comments

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         //variable declarations
-        double maxNormalWeight = 25;
-        double minNormalWeight = 18.5;
+        double maxNormalWeight = 25; //the maximum value for the user to be normal weight
+        double minNormalWeight = 18.5; //the minimum value for the user to be normal weight
 
         System.out.println("\nWhat is your height in inches? "); //ask the user's height
         Scanner userHeightInput = new Scanner(System.in); //scan for the user's height
@@ -26,13 +26,13 @@ public class App {
 
         double BMI = (userWeight / (userHeight * userHeight)) * 703; //calculate the user's BMI
 
-        if ((BMI >= minNormalWeight) && (BMI <= maxNormalWeight)) {
+        if ((BMI >= minNormalWeight) && (BMI <= maxNormalWeight)) { //if the BMI shows that the user is a normal weight
             System.out.println("\nYour BMI is " + String.format("%.1f",BMI) + ".\nYou are within the ideal weight range.");
         }
-        else if (BMI < minNormalWeight) {
+        else if (BMI < minNormalWeight) { //if the BMI shows that the user is underweight
             System.out.println("\nYour BMI is " + String.format("%.1f",BMI) + ".\nYou are underweight. You should go see your doctor.");
         }
-        else {
+        else { //if the BMI shows that the user is overweight
             System.out.println("\nYour BMI is " + String.format("%.1f",BMI) + ".\nYou are overweight. You should go see your doctor.");
         }
     }
